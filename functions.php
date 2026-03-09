a8c0972f4229828fb14efe4307d2355fa4d90d6d
dev-smsumit <sifmsum@gmail.com>
2026-03-10 00:16:36 +0600

    body style add

--- functions.php
+++ functions.php
@@ -1,7 +1,15 @@
 <?php
 add_action('wp_enqueue_scripts', function() {
+    // Parent এর style
     wp_enqueue_style(
         'parent-style',
         get_template_directory_uri() . '/style.css'
+    );
+    
+    // Child এর style
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        ['parent-style']
     );
 });
