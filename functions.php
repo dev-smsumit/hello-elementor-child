ce157a81a38746a577f315158472f884eb000f68
dev-smsumit <sifmsum@gmail.com>
2026-03-10 20:38:59 +0600

    hh

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ add_action('wp_enqueue_scripts', function() {
 add_filter('the_content', function($content) {
     $section = '
     <div class="deploy-section">
-        <p>Deploy Done!</p>
+        
     </div>';
     
     return $content . $section; 
